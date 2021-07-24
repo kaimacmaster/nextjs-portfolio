90316e0e899e2ecde44c3c568bff3ab80086d076
Kai Macmaster <kaimacmaster@me.com>
2021-07-24 15:55:02 +0100

    More refactoring into TypeScript

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,13 @@ import { siteTitle } from "@/lib/constants";
 import utilStyles from "@/styles/utils.module.css";
 
 export default function Home() {
+  const getYearsActive = () => {
+    let diff = (new Date().getTime() - new Date("2011-11-01").getTime()) / 1000;
+    diff /= 60 * 60 * 24;
+
+    return Math.abs(Math.floor(diff / 365.25));
+  };
+
   return (
     <Layout home>
       <Head>
@@ -12,8 +19,8 @@ export default function Home() {
       <section className={utilStyles.headingMd}>
         <p>
           Hello, I&apos;m <strong>Kai</strong>. I&apos;m a full-stack web
-          developer specialising in front-end development with over 9 years
-          working in industry.
+          developer specialising in front-end development with over{" "}
+          {getYearsActive()} years working in industry.
         </p>
         <p>
           Proficient in all stages of development including dev-ops, front-end,
